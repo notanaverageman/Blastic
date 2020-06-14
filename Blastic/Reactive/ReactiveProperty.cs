bceab08a18be50e3a1654488080ae4cb7887bc1b
Yusuf Tarık Günaydın <yusuf_tarik@hotmail.com>
2020-06-14 16:48:10 +0300

    Optimize reactive property.

--- Blastic/Reactive/ReactiveProperty.cs
+++ Blastic/Reactive/ReactiveProperty.cs
@@ -1,22 +1,13 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Reactive.Linq;
-using System.Reactive.Subjects;
 
 namespace Blastic.Reactive
 {
 	public class ReactiveProperty<T> : ReactivePropertyBase<T>, IReactiveProperty<T>
 	{
-		private readonly Subject<T> _source;
-
-		private T _value;
-
-		protected override IObservable<T> Source { get; }
-
 		public T Value
 		{
-			get => _value;
-			set => _source.OnNext(value);
+			get => GetValue();
+			set => SetValue(value);
 		}
 
 		object IReadOnlyReactiveProperty.Value => Value;
@@ -29,31 +20,9 @@ namespace Blastic.Reactive
 		public ReactiveProperty(
 			T initialValue = default,
 			IEqualityComparer<T> equalityComparer = null)
+			:
+			base(initialValue, equalityComparer)
 		{
-			_value = initialValue;
-			_source = new Subject<T>();
-
-			equalityComparer ??= EqualityComparer<T>.Default;
-
-			Source = _source.DistinctUntilChanged(equalityComparer);
-
-			Initialize();
-		}
-
-		public void Dispose()
-		{
-			_source.OnCompleted();
-			_source.Dispose();
-		}
-
-		protected override T GetValue()
-		{
-			return Value;
-		}
-
-		protected override void SetValue(T value)
-		{
-			_value = value;
 		}
 	}
 }
