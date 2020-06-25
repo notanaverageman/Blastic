c7a1892ba4ae1e7d66aba8dcbeb866d8cae30b9a
Yusuf Tarık Günaydın <yusuf_tarik@hotmail.com>
2020-06-25 10:52:23 +0300

    Use reactive properties in diagnostic messages.

--- Blastic/Diagnostics/DiagnosticMessage.cs
+++ Blastic/Diagnostics/DiagnosticMessage.cs
@@ -1,42 +1,58 @@
-﻿using System;
-using System.Threading.Tasks;
 using Blastic.Commanding;
+using Blastic.Reactive;
 
 namespace Blastic.Diagnostics
 {
 	public class DiagnosticMessage
 	{
-		public Severity Severity { get; }
-		public string Message { get; }
+		public IReadOnlyReactiveProperty<Severity> Severity { get; }
+		public IReadOnlyReactiveProperty<string> Message { get; }
 
-		public Func<Task> Action { get; }
-		public string ActionLabel { get; }
+		public AsyncCommand? ActionCommand { get; }
+		public IReadOnlyReactiveProperty<string>? ActionLabel { get; }
 
-		public AsyncCommand ActionCommand { get; }
-
-		public DiagnosticMessage(Severity severity, string message)
+		public DiagnosticMessage(
+			IReadOnlyReactiveProperty<Severity> severity,
+			IReadOnlyReactiveProperty<string> message)
 			:
 			this(severity, message, null, null)
 		{
-			ActionCommand = new AsyncCommand().WithSubscribe(async x =>
-			{
-				if (Action != null)
-				{
-					await Action();
-				}
-			});
+		}
+
+		public DiagnosticMessage(
+			IReadOnlyReactiveProperty<Severity> severity,
+			IReadOnlyReactiveProperty<string> message,
+			AsyncCommand? actionCommand,
+			IReadOnlyReactiveProperty<string>? actionLabel)
+		{
+			Severity = severity;
+			Message = message;
+			ActionCommand = actionCommand;
+			ActionLabel = actionLabel;
+		}
+
+		public DiagnosticMessage(
+			Severity severity,
+			string message)
+			:
+			this(severity, message, null, null)
+		{
 		}
 
 		public DiagnosticMessage(
 			Severity severity,
 			string message,
-			Func<Task> action,
-			string actionLabel)
+			AsyncCommand? actionCommand,
+			string? actionLabel)
+			:
+			this(
+				new ReactiveProperty<Severity>(severity),
+				new ReactiveProperty<string>(message),
+				actionCommand,
+				actionLabel == null
+					? null
+					: new ReactiveProperty<string>(actionLabel))
 		{
-			Severity = severity;
-			Message = message;
-			Action = action;
-			ActionLabel = actionLabel;
 		}
 	}
 }
