89ddb8020424ec64a14bdcd4d79412f85c86827e
Yusuf Tarık Günaydın <yusuf_tarik@hotmail.com>
2020-11-27 12:16:04 +0300

    Add database initialization.

--- Blastic.Forms.Sample/Initialization/Extensions/ApplicationExtensions.cs
+++ Blastic.Forms.Sample/Initialization/Extensions/ApplicationExtensions.cs
@@ -1,6 +1,11 @@
 using System;
+using System.IO;
 using System.Net.Http;
+using Blastic.Data;
+using Blastic.Forms.Data.ProgramData.Migrations;
 using Blastic.Forms.Initialization.Extensions;
+using Blastic.Forms.Sample.Data;
+using Blastic.Forms.Sample.Data.Migrations;
 using Blastic.Forms.Sample.Localization;
 using Blastic.Forms.Sample.UserInterface;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +18,10 @@ namespace Blastic.Forms.Sample.Initialization.Extensions
 	{
 		public static IHostBuilder Initialize(this IHostBuilder hostBuilder, Action<Application> applicationRunner)
 		{
+			string databasePath = Path.Combine(
+				Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+				"Database.sqlite");
+
 			hostBuilder
 				.ConfigureBlasticApplication(
 					applicationBuilder =>
@@ -24,14 +33,31 @@ namespace Blastic.Forms.Sample.Initialization.Extensions
 							.AddShellTab<HomeViewModel>()
 							.AddShellTab<SearchViewModel>()
 							.AddShellTab<LibraryViewModel>()
-							.UseMainViewModel<MainViewModel>();
+							.UseMainViewModel<MainViewModel>()
+							.AddProgramDatabase<ProgramDatabase>(DatabaseProvider.SQLite, $"Data Source={databasePath};");
 					})
+				.AddMigrations()
 				.ConfigureServices(
 					(x, y) =>
 					{
 						y.AddSingleton<Labels>();
 						y.AddSingleton(new HttpClient());
 					});
+
+			return hostBuilder;
+		}
+
+		public static IHostBuilder AddMigrations(this IHostBuilder hostBuilder)
+		{
+			hostBuilder.ConfigureServices((x, y) =>
+			{
+				void AddMigration<T>() where T : ProgramDatabaseMigrationBase
+				{
+					y.AddSingleton<ProgramDatabaseMigrationBase, T>();
+				}
+
+				AddMigration<CreateBooksTable>();
+			});
 
 			return hostBuilder;
 		}
