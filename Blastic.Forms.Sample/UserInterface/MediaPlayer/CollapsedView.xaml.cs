142c2afbca7140602a5256686508e7abe2ffe68f
Yusuf Tarık Günaydın <yusuf_tarik@hotmail.com>
2020-11-27 18:22:30 +0300

    Separate media player views into their own files. Add a renderer to fix input not passing through transparent layouts.

---
+++ Blastic.Forms.Sample/UserInterface/MediaPlayer/CollapsedView.xaml.cs
@@ -0,0 +1,10 @@
+namespace Blastic.Forms.Sample.UserInterface.MediaPlayer
+{
+	public partial class CollapsedView
+	{
+		public CollapsedView()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Blastic.Forms.Sample/UserInterface/MediaPlayer/ExtendedView.xaml.cs
@@ -0,0 +1,10 @@
+namespace Blastic.Forms.Sample.UserInterface.MediaPlayer
+{
+	public partial class ExtendedView
+	{
+		public ExtendedView()
+		{
+			InitializeComponent();
+		}
+	}
+}
