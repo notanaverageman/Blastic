ceb4e4405efe83388fe5d4c56bd0221f31a79c40
Yusuf Tarık Günaydın <yusuf_tarik@hotmail.com>
2020-12-01 13:08:14 +0300

    Remove obsolete classes.

--- Blastic.Forms.Sample/Data/Genre.cs
+++
@@ -1,8 +0,0 @@
-namespace Blastic.Forms.Sample.Data
-{
-	public class Genre
-	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-	}
-}
--- Blastic.Forms.Sample/Data/Reader.cs
+++
@@ -1,8 +0,0 @@
-namespace Blastic.Forms.Sample.Data
-{
-	public class Reader
-	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-	}
-}
