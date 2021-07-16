ef636475660997f325d8eb8b509d62780ac1daf9
Yusuf Tarık Günaydın <yusuf_tarik@hotmail.com>
2021-07-16 10:57:51 +0300

    Add observable extensions.

--- Blastic/Reactive/ObservableExtensions.cs
+++ Blastic/Reactive/ObservableExtensions.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Reactive;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
 
 namespace Blastic.Reactive
 {
@@ -18,6 +21,103 @@ namespace Blastic.Reactive
 		public static IObservable<bool> Or(this IObservable<bool> observable, IObservable<bool> other)
 		{
 			return observable.CombineLatest(other, (x, y) => x || y);
+		}
+		
+		public static IObservable<bool> IsNotNull<T>(this IObservable<T> observable)
+		{
+			return observable.Select(x => x != null);
+		}
+
+		// https://stackoverflow.com/a/65155886/3670437
+		public static IObservable<T> DisposePrevious<T>(this IObservable<T> source) where T : IDisposable
+		{
+			return Observable.Using(
+				() => new SerialDisposable(),
+				serial => source.Do(x => serial.Disposable = x));
+		}
+
+		// https://stackoverflow.com/a/50096940/3670437
+		public static IDisposable SubscribeAsync<T>(
+			this IObservable<T> source,
+			Func<Task> action,
+			Action<Exception>? handler = null)
+		{
+			async Task<Unit> Wrapped(T _)
+			{
+				await action();
+				return Unit.Default;
+			}
+
+			return handler == null
+				? source.SelectMany(Wrapped).Subscribe(_ => { })
+				: source.SelectMany(Wrapped).Subscribe(_ => { }, handler);
+		}
+
+		public static IDisposable SubscribeAsync<T>(
+			this IObservable<T> source,
+			Func<T, Task> action,
+			Action<Exception>? handler = null)
+		{
+			async Task<Unit> Wrapped(T t)
+			{
+				await action(t);
+				return Unit.Default;
+			}
+
+			return handler == null
+				? source.SelectMany(Wrapped).Subscribe(_ => { })
+				: source.SelectMany(Wrapped).Subscribe(_ => { }, handler);
+		}
+
+		// https://stackoverflow.com/a/45217578/3670437
+		public static IObservable<T> DoAsync<T>(
+			this IObservable<T> source,
+			Func<Task> action)
+		{
+			return source
+				.Materialize()
+				.SelectMany(async x =>
+				{
+					switch (x.Kind)
+					{
+						case NotificationKind.OnNext:
+							await action();
+							return x;
+
+						case NotificationKind.OnCompleted:
+						case NotificationKind.OnError:
+							return x;
+
+						default:
+							throw new NotImplementedException();
+					}
+				})
+				.Dematerialize();
+		}
+		
+		public static IObservable<T> DoAsync<T>(
+			this IObservable<T> source,
+			Func<T, Task> action)
+		{
+			return source
+				.Materialize()
+				.SelectMany(async x =>
+				{
+					switch (x.Kind)
+					{
+						case NotificationKind.OnNext:
+							await action(x.Value);
+							return x;
+
+						case NotificationKind.OnCompleted:
+						case NotificationKind.OnError:
+							return x;
+
+						default:
+							throw new NotImplementedException();
+					}
+				})
+				.Dematerialize();
 		}
 	}
 }
