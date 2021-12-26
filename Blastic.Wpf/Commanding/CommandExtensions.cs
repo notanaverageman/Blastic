bf45c703774e0ce467f57058ed979f08cb75cfdc
Yusuf Tarık Günaydın <yusuf_tarik@hotmail.com>
2021-12-26 13:08:42 +0300

    Add more extension methods for WPF input gesture bindings.

--- Blastic.Wpf/Commanding/CommandExtensions.cs
+++ Blastic.Wpf/Commanding/CommandExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Windows;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using Blastic.ViewManagement;
 
@@ -10,35 +11,96 @@ namespace Blastic.Wpf.Commanding
 		public static void AddInputGesture(
 			this ICommand command,
 			InputGesture gesture,
-			IViewAware context = null)
+			IViewAware? context = null)
 		{
-			InputBinding inputBinding = new InputBinding(command, gesture);
+			InputBinding inputBinding = new(command, gesture);
+			AddInputBinding(inputBinding, context);
+		}
 
-			if (context == null)
+		public static void AddInputGesture<T>(
+			this T command,
+			Key key,
+			ModifierKeys modifierKeys = ModifierKeys.None,
+			IViewAware? context = null)
+			where T : ICommand
+		{
+			InputBinding inputBinding = new KeyBinding
 			{
-				Application.Current.MainWindow?.InputBindings.Add(inputBinding);
-				return;
-			}
+				Command = command,
+				Key = key,
+				Modifiers = modifierKeys
+			};
 
-			context.View.Subscribe(x =>
-			{
-				if (!(x is FrameworkElement frameworkElement))
-				{
-					return;
-				}
-
-				frameworkElement.InputBindings.Add(inputBinding);
-			});
+			AddInputBinding(inputBinding, context);
 		}
 
 		public static T WithInputGesture<T>(
 			this T command,
 			InputGesture gesture,
-			IViewAware context = null)
+			IViewAware? context = null)
 			where T : ICommand
 		{
 			command.AddInputGesture(gesture, context);
 			return command;
+		}
+
+		public static T WithInputGesture<T>(
+			this T command,
+			Key key,
+			ModifierKeys modifierKeys = ModifierKeys.None,
+			IViewAware? context = null)
+			where T : ICommand
+		{
+			AddInputGesture(command, key, modifierKeys, context);
+			return command;
+		}
+
+		private static void AddInputBinding(InputBinding inputBinding, IViewAware? context)
+		{
+			if (context == null)
+			{
+				Window? mainWindow = Application.Current.MainWindow;
+
+				if (mainWindow == null)
+				{
+					throw new InvalidOperationException("Main window is not created.");
+				}
+				
+				mainWindow.KeyDown += (_, args) =>
+				{
+					ModifierKeys modifierKeys = Keyboard.Modifiers;
+
+					if (args.OriginalSource is TextBoxBase && modifierKeys is ModifierKeys.None or ModifierKeys.Shift)
+					{
+						return;
+					}
+
+					if (inputBinding.Gesture.Matches(mainWindow, args))
+					{
+						ICommand command = inputBinding.Command;
+						object parameter = inputBinding.CommandParameter;
+
+						if (command.CanExecute(parameter))
+						{
+							command.Execute(parameter);
+						}
+
+						args.Handled = true;
+					}
+				};
+				
+				return;
+			}
+
+			context.View.Subscribe(x =>
+			{
+				if (x is not FrameworkElement frameworkElement)
+				{
+					throw new InvalidOperationException("View does not inherit from FrameworkElement.");
+				}
+
+				frameworkElement.InputBindings.Add(inputBinding);
+			});
 		}
 	}
 }
