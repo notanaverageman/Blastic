3e6ce5d771821c66ec73fb93ef7d9ff31c311bcc
Yusuf Tarık Günaydın <yusuf_tarik@hotmail.com>
2022-08-05 14:26:16 +0300

    Add DialogService stub to Maui project.

---
+++ Blastic.Maui/Services/Dialogs/DialogService.cs
@@ -0,0 +1,27 @@
+﻿using Blastic.Services.Dialogs;
+
+namespace Blastic.Maui.Services.Dialogs
+{
+	public class DialogService : IDialogService
+	{
+		public bool? ShowDialog<T>(object viewModel)
+		{
+			return false;
+		}
+
+		public string ShowOpenFileDialog(FileDialogOptions? options)
+		{
+			return "";
+		}
+
+		public string ShowSaveFileDialog(FileDialogOptions? options)
+		{
+			return "";
+		}
+
+		public string ShowSelectFolderDialog(FileDialogOptions? options)
+		{
+			return "";
+		}
+	}
+}
