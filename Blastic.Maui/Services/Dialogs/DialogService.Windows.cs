45211117e491dbb1c44e76a1971233e3178d2080
Yusuf Tarık Günaydın <yusuf_tarik@hotmail.com>
2022-09-15 11:37:09 +0300

    Add dialog service for Windows.

---
+++ Blastic.Maui/Services/Dialogs/DialogService.Windows.cs
@@ -0,0 +1,61 @@
+﻿using Blastic.Services.Dialogs;
+using System.Windows.Forms;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+using SaveFileDialog = Microsoft.Win32.SaveFileDialog;
+
+namespace Blastic.Maui.Services.Dialogs
+{
+	public class DialogService : IDialogService
+	{
+		public bool? ShowDialog<T>(object viewModel)
+		{
+			return false;
+		}
+
+		public string ShowOpenFileDialog(FileDialogOptions? options)
+		{
+			OpenFileDialog openFileDialog = new()
+			{
+				Filter = options?.Filter?.GetFileDialogRepresentation() ?? "",
+				Multiselect = options?.IsMultiSelect ?? false,
+				InitialDirectory = options?.InitialDirectory ?? ""
+			};
+
+			bool? result = openFileDialog.ShowDialog();
+
+			return result == true
+				? openFileDialog.FileName
+				: "";
+		}
+
+		public string ShowSaveFileDialog(FileDialogOptions? options)
+		{
+			SaveFileDialog saveFileDialog = new()
+			{
+				Filter = options?.Filter?.GetFileDialogRepresentation() ?? "",
+				InitialDirectory = options?.InitialDirectory ?? "",
+				AddExtension = true
+			};
+
+			bool? result = saveFileDialog.ShowDialog();
+
+			return result == true
+				? saveFileDialog.FileName
+				: "";
+		}
+
+		public string ShowSelectFolderDialog(FileDialogOptions? options)
+		{
+			FolderBrowserDialog dialog = new()
+			{
+				SelectedPath = options?.InitialDirectory ?? ""
+			};
+
+			DialogResult result = dialog.ShowDialog();
+
+			return result == DialogResult.OK
+				? dialog.SelectedPath
+				: "";
+		}
+	}
+}
