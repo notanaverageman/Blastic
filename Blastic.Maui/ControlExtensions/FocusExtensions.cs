59e68008c6c534b6e5db4cd956755b14e782bb08
Yusuf Tarık Günaydın <yusuf_tarik@hotmail.com>
2022-09-22 16:35:24 +0300

    Add focus extensions to Maui via reflection.

---
+++ Blastic.Maui/ControlExtensions/FocusExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Blastic.ViewManagement;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace Blastic.Maui.ControlExtensions;
+
+public static class FocusExtensions
+{
+	public static void SetFocus(this IViewAware viewAware, object bindingSource)
+	{
+		if (viewAware.View.Value is not IVisualTreeElement view)
+		{
+			return;
+		}
+
+		VisualElement? element = VisualTreeExtensions.FindChild(view, bindingSource);
+
+		element?.Dispatcher.Dispatch(() =>
+		{
+			element.Focus();
+		});
+	}
+}
---
+++ Blastic.Maui/ControlExtensions/VisualTreeExtensions.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Blastic.Reactive;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace Blastic.Maui.ControlExtensions
+{
+	public static class VisualTreeExtensions
+	{
+		private static readonly Dictionary<Type, HashSet<BindableProperty>> DefaultProperties;
+
+		static VisualTreeExtensions()
+		{
+			DefaultProperties = new Dictionary<Type, HashSet<BindableProperty>>();
+
+			AddDefaultDependencyProperty<Label>(Label.TextProperty);
+			AddDefaultDependencyProperty<Entry>(Entry.TextProperty);
+			AddDefaultDependencyProperty<Button>(Button.CommandProperty);
+		}
+
+		public static T? FindChild<T>(IVisualTreeElement? parent) where T : VisualElement
+		{
+			if (parent == null)
+			{
+				return null;
+			}
+
+			T? foundChild = null;
+			IReadOnlyList<IVisualTreeElement> children = parent.GetVisualChildren();
+
+			foreach (IVisualTreeElement child in children)
+			{
+				if (child is T result)
+				{
+					foundChild = result;
+					break;
+				}
+
+				foundChild = FindChild<T>(child);
+
+				if (foundChild != null)
+				{
+					break;
+				}
+			}
+			
+			return foundChild;
+		}
+
+		public static VisualElement? FindChild(IVisualTreeElement? parent, object source)
+		{
+			if (parent == null)
+			{
+				return null;
+			}
+
+			VisualElement? foundChild = null;
+			IReadOnlyList<IVisualTreeElement> children = parent.GetVisualChildren();
+
+			foreach (IVisualTreeElement child in children)
+			{
+				if (child is not VisualElement element)
+				{
+					continue;
+				}
+
+				foreach (BindableProperty property in GetDefaultDependencyProperties(element))
+				{
+					if (QueryBinding(element, property, source))
+					{
+						return element;
+					}
+				}
+
+				foundChild = FindChild(child, source);
+
+				if (foundChild != null)
+				{
+					break;
+				}
+			}
+
+			return foundChild;
+		}
+
+		private static bool QueryBinding(
+			BindableObject bindable,
+			BindableProperty property,
+			object source)
+		{
+			object? bindingExpression = bindable.GetBinding(property)?.GetBindingExpression();
+			IList? parts = bindingExpression?.GetParts();
+
+			if (parts == null)
+			{
+				return false;
+			}
+
+			object? value = bindable.BindingContext;
+
+			for (int i = 0; i < parts.Count; i++)
+			{
+				if (value == null)
+				{
+					return false;
+				}
+
+				object? part = parts[i];
+
+				if (part == null)
+				{
+					return false;
+				}
+
+				value = part.GetValue(value);
+
+				if (i == parts.Count - 2 && source is IReactiveProperty)
+				{
+					return value == source;
+				}
+			}
+
+			return false;
+		}
+
+		private static IEnumerable<BindableProperty> GetDefaultDependencyProperties(BindableObject bindable)
+		{
+			Type objectType = bindable.GetType();
+			IEnumerable<BindableProperty> result = Enumerable.Empty<BindableProperty>();
+
+			foreach (Type index in DefaultProperties.Keys.Where(x => x.IsAssignableFrom(objectType)))
+			{
+				result = result.Concat(DefaultProperties[index]);
+			}
+
+			return result;
+		}
+
+		public static void AddDefaultDependencyProperty<T>(BindableProperty property)
+		{
+			if (!DefaultProperties.TryGetValue(typeof(T), out HashSet<BindableProperty>? properties))
+			{
+				properties = new HashSet<BindableProperty>();
+				DefaultProperties[typeof(T)] = properties;
+			}
+
+			properties.Add(property);
+		}
+
+		private static Binding? GetBinding(this BindableObject bindable, BindableProperty property)
+		{
+			MethodInfo? methodInfo = typeof(BindableObject)
+				.GetTypeInfo()
+				.GetDeclaredMethod("GetContext");
+
+			object? context = methodInfo?.Invoke(bindable, new object?[] { property });
+
+			FieldInfo? propertyInfo = context?.GetType().GetTypeInfo().GetDeclaredField("Binding");
+			return propertyInfo?.GetValue(context) as Binding;
+		}
+
+		private static object? GetBindingExpression(this Binding self)
+		{
+			FieldInfo? fieldInfo = self.GetType().GetTypeInfo().GetDeclaredField("_expression");
+			return fieldInfo?.GetValue(self);
+		}
+
+		private static IList? GetParts(this object bindingExpression)
+		{
+			FieldInfo? fieldInfo = bindingExpression.GetType().GetTypeInfo().GetDeclaredField("_parts");
+			return fieldInfo?.GetValue(bindingExpression) as IList;
+		}
+
+		private static object? GetValue(this object part, object source)
+		{
+			MethodInfo? methodInfo = part
+				.GetType()
+				.GetTypeInfo()
+				.GetDeclaredMethod("TryGetValue");
+
+			object? value = null;
+			object?[] parameters = { source, value };
+
+			methodInfo?.Invoke(part, parameters);
+			value = parameters[1];
+
+			return value;
+		}
+	}
+}
