4aeef8d3fc102dd3fece132a7e28031adfab7560
Yusuf Tarık Günaydın <yusuf_tarik@hotmail.com>
2023-06-18 16:21:11 +0300

    Use Depso instead of Jab to register services.

---
+++ Blastic.Avalonia/Initialization/BlasticServices.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using Blastic.Avalonia.Platform;
+using Blastic.Avalonia.ViewManagement;
+using Blastic.Platform;
+using Blastic.Services.Localization;
+using Blastic.Services.Messaging;
+using Blastic.Services.Notifications;
+using Blastic.ViewManagement;
+using Depso;
+
+namespace Blastic.Avalonia.Initialization;
+
+[ServiceProviderModule]
+public partial class BlasticServices
+{
+	private static void RegisterServices()
+	{
+		AddSingleton<IEventAggregator, EventAggregator>();
+		AddSingleton<ILocalizationService, LocalizationService>();
+		AddSingleton<INotificationService, NotificationService>();
+		AddSingleton<IPlatformSpecifics, AvaloniaPlatformSpecifics>();
+		AddSingleton<IViewLocator<StyledElement>, ViewLocator>();
+	}
+}
---
+++ Blastic.Maui/Initialization/BlasticServices.cs
@@ -0,0 +1,37 @@
+﻿using Blastic.DynamicControls;
+using Blastic.Maui.DynamicControls;
+using Blastic.Maui.Platform;
+using Blastic.Maui.Services.Dialogs;
+using Blastic.Maui.Services.Navigation;
+using Blastic.Maui.Services.Windowing;
+using Blastic.Maui.ViewManagement;
+using Blastic.Platform;
+using Blastic.Services.Dialogs;
+using Blastic.Services.Localization;
+using Blastic.Services.Messaging;
+using Blastic.Services.Notifications;
+using Blastic.Services.Windowing;
+using Blastic.ViewManagement;
+using Blastic.ViewManagement.TypeMappers;
+using Depso;
+using Microsoft.Maui.Controls;
+
+namespace Blastic.Maui.Initialization;
+
+[ServiceProviderModule]
+public partial class BlasticServices
+{
+	private static void RegisterServices()
+	{
+		AddSingleton<IDialogService, DialogService>();
+		AddSingleton<IEventAggregator, EventAggregator>();
+		AddSingleton<ILocalizationService, LocalizationService>();
+		AddSingleton<INavigationService, NavigationService>();
+		AddSingleton<INotificationService, NotificationService>();
+		AddSingleton<IPlatformSpecifics, MauiPlatformSpecifics>();
+		AddSingleton<IPresenterSource>(_ => PresenterSource.Instance);
+		AddSingleton<ITypeMapper, InheritanceTypeMapper<DynamicModel, DynamicControl>>();
+		AddSingleton<IViewLocator<VisualElement>, ViewLocator>();
+		AddSingleton<IWindowService, WindowService>();
+	}
+}
